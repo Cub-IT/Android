86c2a840b8f149640c8e7fee2c4a6a1a00002129
Mark Yavorskyi <yavorskiymark@gmail.com>
2022-05-27 21:28:59 +0300

    Added: logging interceptor

--- core/src/main/java/com/example/core/data/Retrofit.kt
+++ core/src/main/java/com/example/core/data/Retrofit.kt
@@ -1,5 +1,8 @@
 package com.example.core.data
 
+import okhttp3.Interceptor
+import okhttp3.OkHttpClient
+import okhttp3.logging.HttpLoggingInterceptor
 import retrofit2.Retrofit
 import retrofit2.converter.gson.GsonConverterFactory
 
@@ -7,4 +10,16 @@ val BaseRetrofit by lazy { createRetrofit("HERE GOES BASE URL").build() }
 
 private fun createRetrofit(url: String) = Retrofit.Builder()
     .baseUrl(url)
+    .client(createOkHttpClient())
     .addConverterFactory(GsonConverterFactory.create())
+
+private fun createOkHttpClient(): OkHttpClient {
+    return OkHttpClient.Builder()
+        .addInterceptor(createLoggingInterceptor())
+        .build()
+}
+
+private fun createLoggingInterceptor(): Interceptor {
+    return HttpLoggingInterceptor()
+        .setLevel(HttpLoggingInterceptor.Level.BODY)
+}
