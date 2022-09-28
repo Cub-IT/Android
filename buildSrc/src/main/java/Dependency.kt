550d6041c5dbcb7ba9ad78de89e77cca2dc9a8af
Mark Yavorskyi <yavorskiymark@gmail.com>
2022-09-28 12:02:05 +0300

    migrated settings.gradle (app module) to kts

--- buildSrc/src/main/java/Dependency.kt
+++ buildSrc/src/main/java/Dependency.kt
@@ -1,24 +1,37 @@
 object Dependency {
-
     object Plugin {
         // Don't forget to update deps at buildSrc (build.gradle.kts)
 
-        val kotlinGradlePlugin = "org.jetbrains.kotlin:kotlin-gradle-plugin:${Version.Kotlin.kotlinVersion}"
-        val kotlinAndroidExtensionsPlugin = "org.jetbrains.kotlin:kotlin-android-extensions:${Version.Kotlin.kotlinVersion}"
+        val kotlinGradlePlugin = "org.jetbrains.kotlin:kotlin-gradle-plugin:${Version.Kotlin.kotlin}"
+        val kotlinAndroidExtensionsPlugin = "org.jetbrains.kotlin:kotlin-android-extensions:${Version.Kotlin.kotlin}"
 
-        val androidGradlePlugin = "com.android.tools.build:gradle:${Version.Plugin.androidGradlePluginVersion}"
+        val androidGradlePlugin = "com.android.tools.build:gradle:${Version.Plugin.androidGradlePlugin}"
     }
 
     object Kotlin {
-        val kotlinStdLib = "org.jetbrains.kotlin:kotlin-stdlib-jdk7:${Version.Kotlin.kotlinVersion}"
-        val coroutines = "org.jetbrains.kotlinx:kotlinx-coroutines-android:${Version.Kotlin.coroutinesVersion}"
+        val kotlinStdLib = "org.jetbrains.kotlin:kotlin-stdlib-jdk7:${Version.Kotlin.kotlin}"
+        val coroutines = "org.jetbrains.kotlinx:kotlinx-coroutines-android:${Version.Kotlin.coroutines}"
+    }
+
+    object Ui {
+        val coreKtx = "androidx.core:core-ktx:${Version.Ui.coreKtx}"
+        val lifecycleRuntimeKtx = "androidx.lifecycle:lifecycle-runtime-ktx:${Version.Ui.lifecycleRuntimeKtx}"
+        val activityCompose = "androidx.activity:activity-compose:${Version.Ui.activityCompose}"
     }
 
     object Compose {
-        val ui = "androidx.compose.ui:ui:${Version.Compose.composeVersion}"
-        val material = "androidx.compose.material:material:${Version.Compose.composeVersion}"
-        val uiTooling = "androidx.compose.ui:ui-tooling:${Version.Compose.composeVersion}"
-        val uiToolingPreview = "androidx.compose.ui:ui-tooling-preview:${Version.Compose.composeVersion}"
-        val livedata = "androidx.compose.runtime:runtime-livedata:${Version.Compose.composeVersion}"
+        val ui = "androidx.compose.ui:ui:${Version.Compose.compose}"
+        val material = "androidx.compose.material:material:${Version.Compose.compose}"
+        val uiTooling = "androidx.compose.ui:ui-tooling:${Version.Compose.compose}"
+        val uiToolingPreview = "androidx.compose.ui:ui-tooling-preview:${Version.Compose.compose}"
+        val livedata = "androidx.compose.runtime:runtime-livedata:${Version.Compose.compose}"
+        val uiTestJUnit4 = "androidx.compose.ui:ui-test-junit4:${Version.Compose.compose}"
+        val uiTestManifest = "androidx.compose.ui:ui-test-manifest:${Version.Compose.compose}"
+    }
+
+    object Test {
+        val junit = "junit:junit:${Version.Test.junit}"
+        val extJUnit = "androidx.test.ext:junit:${Version.Test.extJUnit}"
+        val espressoCore = "androidx.test.espresso:espresso-core:${Version.Test.espressoCore}"
     }
 }
--- buildSrc/src/main/java/Version.kt
+++ buildSrc/src/main/java/Version.kt
@@ -1,22 +1,39 @@
-class Version {
-
+object Version {
     object App {
-        val minSdkVersion = 24
-        val targetSdkVersion = 32
-        val compileSdkVersion = targetSdkVersion
+        val namespace = "ua.university.cubit"
+        val applicationId = "ua.university.cubit"
+
+        val minSdk = 24
+        val targetSdk = 32
+        val compileSdk = targetSdk
+
+        val versionCode = 1
+        val versionName = "1.0"
     }
 
     object Plugin {
-        val androidGradlePluginVersion = "7.3.0"
+        val androidGradlePlugin = "7.3.0"
     }
 
     object Kotlin {
-        val kotlinVersion = "1.7.10"
-        val coroutinesVersion = "1.6.4"
+        val kotlin = "1.7.10"
+        val coroutines = "1.6.4"
+    }
+
+    object Ui {
+        val coreKtx = "1.7.0"
+        val lifecycleRuntimeKtx = "2.3.1"
+        val activityCompose = "1.3.1"
     }
 
     object Compose {
-        val composeVersion = "1.2.0"
-        val composeCompilerVersion = "1.3.0"
+        val compose = "1.2.0"
+        val composeCompiler = "1.3.0"
+    }
+
+    object Test {
+        val junit = "4.13.2"
+        val extJUnit = "1.1.3"
+        val espressoCore = "3.4.0"
     }
 }
