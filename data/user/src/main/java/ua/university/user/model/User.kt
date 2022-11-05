aa7ab9a92ae1de54211c6bead6afa6220a2bb96b
Mark Yavorskyi <yavorskiymark@gmail.com>
2022-11-05 21:09:02 +0200

    refactored User domain model

--- data/user/src/main/java/ua/university/user/model/User.kt
+++ data/user/src/main/java/ua/university/user/model/User.kt
@@ -3,21 +3,25 @@ package ua.university.user.model
 import androidx.compose.ui.graphics.Color
 
 data class User(
-    val id: String,
+    val id: Int,
+    val participantIds: List<Int>,
+
     val firstName: String,
     val lastName: String,
     val email: String,
-    val imageUrl: String,
+    val avatarUrl: String,
     val color: Color,
 )
 
 fun previewUser(key: Int = 22, color: Color = Color.Magenta): User {
     return User(
-        id = "123456abc$key",
+        id = key,
+        participantIds = emptyList(),
+
         firstName = "Alex",
         lastName = "Vanyo",
         email = "petro.ivanov@gmail.com",
-        imageUrl = "https://pbs.twimg.com/profile_images/1431339735931305989/nOE2mmi2_400x400.jpg",
+        avatarUrl = "https://pbs.twimg.com/profile_images/1431339735931305989/nOE2mmi2_400x400.jpg",
         color = color,
     )
 }
